ecfcd8ec6116d24cb746ce5af3dd875103696121
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-18 13:27:25 +0100

    refactor so if/else is inside a .forEach loop

--- spec/SoundWaveSpec.js
+++ spec/SoundWaveSpec.js
@@ -4,19 +4,19 @@ describe ("soundWaveFilter", function (){
     expect(soundWaveFilter([20], 10, 30)).toEqual([20])
   })
 
-  it ("returns a single soundwave modified when the lower limit is breached", function (){
+  xit ("returns a single soundwave modified when the lower limit is breached", function (){
     expect(soundWaveFilter([9], 10, 30)).toEqual([10])
   })
 
-  it ("returns a single soundWave modified when the upper limit is reached", function (){
+  xit ("returns a single soundWave modified when the upper limit is reached", function (){
     expect(soundWaveFilter([31], 10, 30)).toEqual([30])
   })
 
-  it ("returns multiple soundwaves unchanged when the upper and lower limits are not reached", function (){
+  xit ("returns multiple soundwaves unchanged when the upper and lower limits are not reached", function (){
     expect(soundWaveFilter([20, 20, 20, 20], 10, 30)).toEqual([20, 20, 20, 20])
   })
 
-  it ("returns multiple soundwaves modified when the lower limit is breached twice", function (){
+  xit ("returns multiple soundwaves modified when the lower limit is breached twice", function (){
     expect(soundWaveFilter([9, 9], 10, 30)).toEqual ([10, 10])
   })
 });
--- src/soundWave.js
+++ src/soundWave.js
@@ -1,16 +1,17 @@
 function soundWaveFilter(soundWave, lowerLimit, upperLimit){
   let result = []
-  if(soundWave < lowerLimit){
     soundWave.forEach(frequency => {
+      if(frequency < lowerLimit){
        frequency = lowerLimit
       result.push(frequency)
-    });
-  }else if(soundWave > upperLimit) {
-    result.push(upperLimit)
-   } else {
-    soundWave.forEach(frequency => {
-      result.push(frequency)
-    })
-  }
-  return result
-};
+    }
+    else if(soundWave > upperLimit){
+      result.push(upperLimit)
+     } 
+     else{
+        result.push(frequency)
+      }
+   
+  })
+  return result;
+}
