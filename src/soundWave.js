074d9f1997d945552e1f309fc214a7732e2e761a
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-18 10:56:39 +0100

    green test -returns a single soundWave modified when the upper limit is reached

--- src/soundWave.js
+++ src/soundWave.js
@@ -1,7 +1,10 @@
-function soundWaveFilter(soundWave, lowerLimit){
+function soundWaveFilter(soundWave, lowerLimit, upperLimit){
   let result = []
   if(soundWave < lowerLimit){
     result.push(lowerLimit)
+    return result
+  }else if(soundWave > upperLimit) {
+    result.push(upperLimit)
     return result
   } else {
   return soundWave
