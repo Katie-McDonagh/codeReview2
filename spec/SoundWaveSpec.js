c0fb2c8039aa5f2e41e3720a177b66596b4bc07d
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-18 13:46:20 +0100

    red test -returns multiple soundwaves modified when the upper limit is breached

--- spec/SoundWaveSpec.js
+++ spec/SoundWaveSpec.js
@@ -4,19 +4,23 @@ describe ("soundWaveFilter", function (){
     expect(soundWaveFilter([20], 10, 30)).toEqual([20])
   })
 
-  xit ("returns a single soundwave modified when the lower limit is breached", function (){
+  it ("returns a single soundwave modified when the lower limit is breached", function (){
     expect(soundWaveFilter([9], 10, 30)).toEqual([10])
   })
 
-  xit ("returns a single soundWave modified when the upper limit is reached", function (){
+  it ("returns a single soundWave modified when the upper limit is reached", function (){
     expect(soundWaveFilter([31], 10, 30)).toEqual([30])
   })
 
-  xit ("returns multiple soundwaves unchanged when the upper and lower limits are not reached", function (){
+  it ("returns multiple soundwaves unchanged when the upper and lower limits are not reached", function (){
     expect(soundWaveFilter([20, 20, 20, 20], 10, 30)).toEqual([20, 20, 20, 20])
   })
 
-  xit ("returns multiple soundwaves modified when the lower limit is breached twice", function (){
+  it ("returns multiple soundwaves modified when the lower limit is breached twice", function (){
     expect(soundWaveFilter([9, 9], 10, 30)).toEqual ([10, 10])
+  })
+
+  it ("returns multiple soundwaves modified when the upper limit is breached", function (){
+    expect(soundWaveFilter([31, 31], 10, 30)).toEqual ([30, 30])
   })
 });
