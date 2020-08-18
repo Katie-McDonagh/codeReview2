6f7b319407a1dfd060a350f054a35931c8523717
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-18 12:07:50 +0100

    red test -returns multiple soundwaves modified when the lower limit is breached twice

--- spec/SoundWaveSpec.js
+++ spec/SoundWaveSpec.js
@@ -15,4 +15,8 @@ describe ("soundWaveFilter", function (){
   it ("returns multiple soundwaves unchanged when the upper and lower limits are not reached", function (){
     expect(soundWaveFilter([20, 20], 10, 30)).toEqual([20, 20])
   })
+
+  it ("returns multiple soundwaves modified when the lower limit is breached twice", function (){
+    expect(soundWaveFilter([9, 9], 10, 30)).toEqual ([10, 10])
+  })
 });
