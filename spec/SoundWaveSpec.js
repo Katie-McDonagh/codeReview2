0de16db6a45fdd4124921d86f466fffeaae5b301
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-18 10:44:20 +0100

    step back to simplify TDD process

--- spec/SoundWaveSpec.js
+++ spec/SoundWaveSpec.js
@@ -1,7 +1,7 @@
 describe ("soundWaveFilter", function (){
 
   it ("returns a soundwave unchanged when the upper and lower limits are not reached", function (){
-    expect(soundWaveFilter([20, 20, 20], 10, 30)).toEqual([20, 20, 20])
+    expect(soundWaveFilter([20], 10, 30)).toEqual([20])
   });
 
 })
--- src/soundWave.js
+++
@@ -1,3 +0,0 @@
-function soundWaveFilter (soundWaves) {
-  return soundWaves
-}
