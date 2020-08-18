3392b09588917169528416ba963560438809a582
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-18 10:59:08 +0100

    refactor to return result outside of the if statment

--- src/soundWave.js
+++ src/soundWave.js
@@ -2,11 +2,10 @@ function soundWaveFilter(soundWave, lowerLimit, upperLimit){
   let result = []
   if(soundWave < lowerLimit){
     result.push(lowerLimit)
-    return result
   }else if(soundWave > upperLimit) {
     result.push(upperLimit)
-    return result
   } else {
-  return soundWave
+  result.push(soundWave[0])
   }
+  return result
 };
